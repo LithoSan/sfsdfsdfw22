291f1d30ad1bab149afecc663e08c9a9f64f491a
LithoSan <51868263+LithoSan@users.noreply.github.com>
2019-07-08 18:30:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "+";
-var adminprefix = '-'
+var adminprefix = 'Y'
 
 
 const developers = ["461314530438545418"]
